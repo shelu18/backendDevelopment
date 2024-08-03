0756bd847ad577f8766e7ebd652df2fda4d3d769
shailendra parihar <shailendraparihar3630@gmail.com>
2024-08-03 23:32:48 +0530

    app configuration via express and cors

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";   //it is used to access the cookes of browser from the server and set the cookes in the clients browsser 
+
+// Create Express server
+const app = express();
+
+app.use(cors({
+    origin:ProcessingInstruction.env.CORS_ORIGIN,
+     credentials : true
+}))
+app.use(express.json({
+    limit:"16kb"
+}))                    // it means i am accepting data in json format and theat option is object under json define under express to set the limit of the data that we are accepting
+app.use (express.urlencoded({extended:true,
+    limit:"16kb"
+}))  //it is used to accept the data in the form of urlencoded data  url extended true means we can further extend the data
+
+app.use(express.static("public"));  //it is used to serve the static files from the public folder this configuration is gejnerally for the keeping the data locally at the server and it is used to serve the static files from the public folder
+
+app.use(cookieParser())
+export default app;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import mongoose from 'mongoose';
 
 import connectDB from './db/index.js';
 
-connectDB();
+connectDB()        
+ .then (()=>{
+   
+    app.listen(process.env.PORT ||8000,()=>{    //here we used the listner to listen the port at the real time from the port if tha is not available it use the  default port number 8000 
+        console.log(`server is running on port ${process.env.PORT ||8000}`);
+
+    })
+ })       //when database will connecr it will return the promise 
+ .catch((err)  => {
+    console.log("connection failed",err);
+ })                
