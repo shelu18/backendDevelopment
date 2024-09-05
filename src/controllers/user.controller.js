9a9d0c660167d9731d294c7de163e2fd33a8f0f6
shailendra parihar <shailendraparihar3630@gmail.com>
2024-09-05 22:45:23 +0530

    writing logic for the user registration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,16 @@
-import {asyncHandler}  from "../utils/asyncHandlers.js";
+import { asyncHandler } from "../utils/asyncHandlers.js";
 
-const registerUser =asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
-})
-export { registerUser}
+// Steps of user registration:
+// 1. Get details from frontend
+// 2. Validate all fields
+// 3. Check if user already exists
+// 4. Upload files to Cloudinary
+// 5. Save the data to MongoDB and return the response
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, username, password } = req.body;
+  console.log("email", email);
+  // Add the rest of the registration logic here
+});
+
+export { registerUser };
